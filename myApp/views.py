da005ea616528e7b21a07727ad20be61b3f319c4
Daniel <danielbrotchford@gmail.com>
2022-11-24 14:58:23 +0000

    adding creation page

--- myApp/views.py
+++ myApp/views.py
@@ -18,3 +18,19 @@ def index(request):
     data = Projects.objects.all()
     # return HttpResponse("Hello CS3001 Class, this is our index page")
     return render(request, "index.html", {"data" : data})
+
+def create(request):
+
+    return render(request, "create.html")
+
+
+def addData(request):
+    project_name = request.POST['project_name']
+    student_name = request.POST['student_name']
+    project_description = request.POST['project_description']
+
+    entry = Projects.objects.create(project_name=project_name, student_name=student_name, project_description=project_description)
+    data = Projects.objects.all()
+    
+    # return HttpResponse("Hello CS3001 Class, this is our index page")
+    return render(request, "index.html", {"data" : data})
